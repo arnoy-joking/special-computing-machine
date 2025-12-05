93f6ec98e467c2a4fe0511caeb0cf5d987b113ee
Arnoy Goswami <arnoy799@gmail.com>
2025-12-05 18:37:21 +0000

    after doing the ctrl shift r 5 times, it shows the favicon. then reload,

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -3,7 +3,6 @@
 
 import AppSidebar from '@/components/layout/app-sidebar';
 import PlayerFooter from '@/components/layout/player-footer';
-import { ClientProviders } from '@/components/client-providers';
 import VideoPlayer from '@/components/player/video-player';
 import QueuePanel from '@/components/layout/queue-panel';
 import { useUIStore } from '@/lib/store';
@@ -18,7 +17,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   const isQueueOpen = useUIStore((state) => state.isQueueOpen);
-  const { isSidebarOpen, toggleSidebar, isSidebarCollapsed } = useUIStore();
+  const { isSidebarOpen, toggleSidebar } = useUIStore();
   const pathname = usePathname();
 
   useEffect(() => {
@@ -30,12 +29,12 @@ export default function MainLayout({
   }, [isSidebarOpen]);
 
   return (
-    <ClientProviders>
+    <>
       <VideoPlayer />
       <div className="flex flex-1 overflow-hidden relative">
          {isSidebarOpen && <div onClick={toggleSidebar} className="sm:hidden fixed inset-0 bg-black/60 z-20" />}
         <AppSidebar />
-        <div className="flex-1 flex flex-col overflow-hidden pb-20">
+        <div className="flex-1 flex flex-col overflow-hidden pb-20 sm:pb-0">
           <MobileHeader />
           <main className="flex-1 flex flex-col bg-background overflow-hidden">
             <section id="content-section" className="flex-1 overflow-y-auto custom-scrollbar p-4 sm:p-6 lg:p-8">
@@ -54,6 +53,6 @@ export default function MainLayout({
         <QueuePanel />
       </div>
       <PlayerFooter />
-    </ClientProviders>
+    </>
   );
 }
