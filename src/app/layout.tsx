32ed2398d21ebae990fda4985d3919ede8bbcac6
Arnoy Goswami <arnoy799@gmail.com>
2025-12-05 18:14:17 +0000

    doesnt work. force it to show the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="https://www.nicepng.com/png/full/358-3582537_best-apollo-icon-android-lollipop-png-play-music.png" type="image/png" />
+        <link rel="icon" href="https://www.nicepng.com/png/full/358-3582537_best-apollo-icon-android-lollipop-png-play-music.png" type="image/png" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
